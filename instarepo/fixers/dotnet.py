2a8420eb4c819e38777b226f76661df0af8d88c1
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-11-17 08:23:56 +0100

    DotNetFrameworkVersionFix set to work only on csproj files referenced by sln files (Fixes #17)

--- instarepo/fixers/dotnet.py
+++ instarepo/fixers/dotnet.py
@@ -1,34 +1,14 @@
+"""Fixers for .NET projects"""
 import logging
 import os
 import os.path
 import xml.etree.ElementTree as ET
+from typing import Iterable
+
 import instarepo.git
 import instarepo.xml_utils
 from instarepo.fixers.base import MissingFileFix
-
-
-def is_csharp_project(directory: str) -> bool:
-    """
-    Checks if the given directory is a C# project.
-    The directory must have a .sln file which references at least one C# project.
-    """
-    with os.scandir(directory) as it:
-        for entry in it:
-            if entry.name.endswith(".sln") and entry.is_file():
-                return is_csharp_solution(entry.path)
-    return False
-
-
-def is_csharp_solution(sln_file: str) -> bool:
-    """
-    Checks if the given sln file references C# projects.
-    """
-    with open(sln_file, "r") as f:
-        lines = f.readlines()
-        # e.g. Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "CVRender", "CVRender\CVRender.csproj", "{BD17C766-DF9E-4117-A8CB-2BAA8FE6D9B9}"
-        project_lines = [line for line in lines if line.startswith("Project(")]
-        cs_proj_lines = [line for line in project_lines if ".csproj" in line]
-        return len(cs_proj_lines) > 0
+from .finders import is_file_of_extension
 
 
 class DotNetFrameworkVersionFix:
@@ -41,14 +21,24 @@ class DotNetFrameworkVersionFix:
     def run(self):
         # ensure abspath so that dirpath in the loop is also absolute
         self.result = []
-        for entry in os.walk(os.path.abspath(self.git.dir)):
-            dirpath, dirnames, filenames = entry
-            for filename in filenames:
-                if filename.endswith(".csproj"):
-                    self.process_csproj(os.path.join(dirpath, filename))
-                elif filename.lower() == "web.config":
-                    self.process_web_config(os.path.join(dirpath, filename))
+        with os.scandir(self.git.dir) as iterator:
+            for entry in iterator:
+                if is_file_of_extension(entry, ".sln"):
+                    self.process_sln(entry.path)
         return self.result
+
+    def process_sln(self, sln_path: str):
+        for relative_csproj in get_projects_from_sln_file(sln_path):
+            parts = relative_csproj.replace("\\", "/").split("/")
+            abs_csproj = os.path.join(self.git.dir, *parts)
+            self.process_csproj(abs_csproj)
+            directory_parts = parts[0:-1]
+            csproj_dir = os.path.join(self.git.dir, *directory_parts)
+            self.process_web_configs(csproj_dir)
+
+    def process_web_configs(self, csproj_dir: str):
+        for web_config in get_web_configs_from_dir(csproj_dir):
+            self.process_web_config(web_config)
 
     def process_csproj(self, filename: str):
         logging.debug("Processing csproj %s", filename)
@@ -111,14 +101,30 @@ class MustHaveCSharpAppVeyor(MissingFileFix):
     def __init__(self, git: instarepo.git.GitWorkingDir, **kwargs):
         super().__init__(git, "appveyor.yml")
 
-    def should_process_repo(self):
-        return is_csharp_project(self.git.dir)
+    def should_process_repo(self) -> bool:
+        """
+        Checks if the repo should be processed.
+        The repo should be processed if it contains exactly one sln file
+        at the root directory which references at least one csproj file.
+        """
+        sln_path = ""
+        with os.scandir(self.git.dir) as iterator:
+            for entry in iterator:
+                if is_file_of_extension(entry, ".sln"):
+                    if sln_path:
+                        # multiple sln files not supported currently
+                        return False
+                    else:
+                        sln_path = entry.path
+        if not sln_path:
+            return False
+        return len(get_projects_from_sln_file(sln_path)) > 0
 
     def get_contents(self):
         return """version: 1.0.{build}
 assembly_info:
   patch: true
-  file: '**\AssemblyInfo.*'
+  file: '**\\AssemblyInfo.*'
   assembly_version: '{version}'
   assembly_file_version: '{version}'
   assembly_informational_version: '{version}'
@@ -127,3 +133,43 @@ before_build:
 build:
   verbosity: minimal
 """
+
+
+def get_projects_from_sln_file(path: str) -> list[str]:
+    """
+    Gets the projects defined in a sln file.
+
+    :param path: The path of a Visual Studio sln file.
+    """
+    with open(path, "r", encoding="utf-8") as file:
+        return list(get_projects_from_sln_file_contents(file.read()))
+
+
+def get_projects_from_sln_file_contents(contents: str) -> Iterable[str]:
+    """
+    Gets the projects defined in a sln file.
+
+    :param contents: The contents of a Visual Studio sln file.
+    """
+    for line in contents.splitlines():
+        if line.startswith('Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}")'):
+            parts = line.split(",")
+            csproj = parts[1].strip()
+            if csproj.startswith('"'):
+                csproj = csproj[1:].strip()
+            if csproj.endswith('"'):
+                csproj = csproj[:-1].strip()
+            yield csproj
+
+
+def get_web_configs_from_dir(csproj_dir: str) -> Iterable[str]:
+    """
+    Gets the files named web.config in a directory.
+    In a case-sensitive file system, if the directory contains both
+    web.config and Web.Config, both will be returned.
+    The iterator yields absolute paths.
+    """
+    with os.scandir(csproj_dir) as iterator:
+        for entry in iterator:
+            if entry.is_file() and entry.name.lower() == "web.config":
+                yield entry.path
