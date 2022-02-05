073a0dbb79ed8b6ef5320f9f193e735ed2287c0e
Nikolaos Georgiou <Nikolaos.Georgiou@gmail.com>
2022-02-05 18:01:19 +0100

    chore(release): prepare for next iteration

--- instarepo/main.py
+++ instarepo/main.py
@@ -9,7 +9,7 @@ import instarepo.commands.list
 import instarepo.commands.login
 import instarepo.commands.logout
 
-VERSION = "0.5.0"
+VERSION = "0.6.0-alpha"
 
 def main():
     """Main entrypoint of the program"""
